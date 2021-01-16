2bc7b040bc084fd9ab23e329f3fd78ac8190eac8
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-01-17 00:59:11 +0200

    added restData.js, json server@0.16.0,http crud requests + header,errorhandling-page not found

--- exampleApp/src/app/model/model.module.ts
+++ exampleApp/src/app/model/model.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { StaticDataSource } from './static.datasource';
+//import { StaticDataSource } from './static.datasource';
 import { Model } from './model.repository';
+import { HttpClientModule } from '@angular/common/http'
+import { RestDataSource, REST_URL } from './rest.datasource';
 
 @NgModule({
   providers:[
-      StaticDataSource,
-      Model
+      Model,
+      RestDataSource,
+      { provide: REST_URL, useValue: `http://${location.hostname}:3500/products` }
     ],  
   declarations: [],
-  imports: []
+  imports: [
+    HttpClientModule
+  ]
 })
 export class ModelModule { }
